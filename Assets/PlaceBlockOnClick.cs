f3cbb07e21c6547fa2c6c65308b1bb070a251f61
ukiwacrash <154051150+ukiwacrash@users.noreply.github.com>
2024-06-14 22:36:37 +0900

    spawntimer

--- Assets/PlaceBlockOnClick.cs
+++ Assets/PlaceBlockOnClick.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class PlaceBlockOnClick : MonoBehaviour
 {
     public GameObject blockPrefab; // ドラッグアンドドロップで設定するブロックのプレハブ
+    public float timerMax = 1.0f; // 生成間隔（秒）
+    float timer = 0f; // タイマー
 
-    
     void Start()
     {
-      
+
     }
     void Update()
     {
+        timer -= Time.deltaTime;
 
         if (Input.GetMouseButtonDown(0)) // 左クリックを検出
         {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePosition.z = 0f; // 2D座標に設定
-
-            Instantiate(blockPrefab, mousePosition, Quaternion.identity);
-
            
+
+            if (timer < 0f)
+            {
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosition.z = 0f; // 2D座標に設定
+
+                Instantiate(blockPrefab, mousePosition, Quaternion.identity);
+
+                timer = timerMax;
+            }
         }
     }
+  
+    
 }
