34f9903efebc923c4f1e9e49ee111647fe6c7aee
Kenzo1130 <tani.kenzo1130@gmail.com>
2024-07-07 22:44:37 +0900

    neko
    ネコスクリプトつくりました

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class PlayerLifeController : MonoBehaviour
 {
-    public GameObject Life_1;
-    public GameObject Life_2;
-    public GameObject Life_3;
-    public GameObject Life_4;
-    public GameObject Life_5;
+    public GameObject[] Life;
     public GameObject textGameOver;
 
     private int life = 5;
@@ -27,35 +23,12 @@ public class PlayerLifeController : MonoBehaviour
             life--;
             //“G‚ð”j‰ó‚·‚é
             Destroy(collision.gameObject);
-            if (life == 4)
+            for(int i = 0;i < Life.Length; i++)
             {
-                Life_5.SetActive(false);
+                Life[i].SetActive(i < life);
             }
-            else if (life == 3)
+            if (life == 0)
             {
-                Life_5.SetActive(false);
-                Life_4.SetActive(false);
-            }
-            else if (life == 2)
-            {
-                Life_5.SetActive(false);
-                Life_4.SetActive(false);
-                Life_3.SetActive(false);
-            }
-            else if (life == 1)
-            {
-                Life_5.SetActive(false);
-                Life_4.SetActive(false);
-                Life_3.SetActive(false);
-                Life_2.SetActive(false);
-            }
-            else if (life == 0)
-            {
-                Life_5.SetActive(false);
-                Life_4.SetActive(false);
-                Life_3.SetActive(false);
-                Life_2.SetActive(false);
-                Life_1.SetActive(false);
                 ShowGameOver();
             }
         }
