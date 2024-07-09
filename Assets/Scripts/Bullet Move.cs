01e0e200da42e4495328e0d3e9db59a034313f39
Hitomi2213 <h.tanaka1053@gmail.com>
2024-07-09 22:34:04 +0900

    Bullet

---
+++ Assets/Scripts/Bullet Move.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+
+    [SerializeField]
+    float movespeed = 2.0f;     //’e‚Ì‘¬‚³
+
+    public Vector3 movement = new Vector3(-1.0f, 0.0f, 0.0f);    //ˆÚ“®•ûŒü
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SetMoveSpeed(float _speed)
+    {
+        
+    }
+
+    public void SetMoveVec(Vector3 _vec)
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+}
