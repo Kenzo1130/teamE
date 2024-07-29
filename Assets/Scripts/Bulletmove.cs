c8ec0e61958be44d96dc26d75fb6add3e89f8d4f
ukiwacrash <154051150+ukiwacrash@users.noreply.github.com>
2024-07-29 22:42:49 +0900

    a

--- Assets/Scripts/Bulletmove.cs
+++ Assets/Scripts/Bulletmove.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class Bulletmove : MonoBehaviour
 {
-    [SerializeField] public float speed = -1.0f; //弾のは速さ
+    [SerializeField,Header("弾の速さ")] float Speed; //弾のは速さ
+    [SerializeField,Header("弾の威力")] float Power; //弾の威力
+    [SerializeField, Header("跳ね返りの強さ")] float magnification; //跳ね返った弾のスピードに掛ける倍率
+
+    private Rigidbody2D Rigid;
+    Vector3 direction = Vector3.down;
+
 
     // Start is called before the first frame update
     void Start()
     {
-      
+      Rigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime); // y軸方向に移動
-                                                                  
-       
+        Move();
     }
 
-   
+    private void Move()
+    {
+        Rigid.velocity = direction * Speed;     //オブジェクトの向きに速さをかける計算
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)  //オブジェクトに接触したら呼び出される
+    {
+        direction = collision.transform.up;     //触れたオブジェクトに対して垂直に跳ね返る
+        Speed *= magnification;     //オブジェクトに触れた後はスピードが５倍の速さになる
+    }
 }
