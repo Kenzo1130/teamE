5aa4b23b71ac27307615f170e1eef2933dde31a4
Kenzo1130 <tani.kenzo1130@gmail.com>
2024-07-31 14:30:11 +0900

    BGMの追加

---
+++ Assets/Scripts/PlayerStand.cs
@@ -0,0 +1,65 @@
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerStand : MonoBehaviour
+{
+    public GameObject[] Life;
+    public GameObject textGameOver;
+    public AudioClip DamageSound;
+    public AudioClip HealSound;
+    AudioSource audioSource;
+    
+
+    int life;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        life = Life.Length;
+        textGameOver.SetActive(false);
+    }
+    void Update()
+    {
+        for (int i = 0; i < Life.Length; i++)
+        {
+            Life[i].SetActive(i < life);
+        }
+        if (life == 0)
+        {
+            ShowGameOver();
+        }
+    }
+
+    //“G‚ÆÕ“Ë‚µ‚½‚Æ‚«‚Ìˆ—
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            //Žc‹@‚ðŒ¸‚ç‚·
+            life--;
+            audioSource.PlayOneShot(DamageSound);
+        }  
+        
+        if (collision.CompareTag("Bullet"))
+        {
+           Destroy(collision.gameObject);
+        }
+    }
+
+    void ShowGameOver()
+    {
+        textGameOver.SetActive(true);
+    }
+    public void Heal(int healAmount)
+    {
+        audioSource.PlayOneShot(HealSound);
+        life += healAmount;
+        if (life > Life.Length) life = Life.Length;
+    }
+}
+
+
+
